16c2604f2baeb88d4235b1c234ed08b7ade55439
AnkushS27 <ankush27singh11@gmail.com>
2025-05-29 23:30:15 +0530

    add shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       Home page
+      <Button variant="default" size="lg" className="mt-4">
+        Click Me
+      </Button>
     </div>
   )
 }
